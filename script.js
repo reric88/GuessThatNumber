70a6c4ee0b4841d1018ec2d68208255ed260af4c
Eric Collard <ruffyreborn@gmail.com>
2023-03-29 10:12:02 -0500

    defined the variables to be used within the javascript document

---
+++ script.js
@@ -0,0 +1,48 @@
+console.log('ᓚᘏᗢ');
+// Guess That Number
+// Message to be used throughout the project
+const enterNumText = 'Please enter a number greate than zerp:';
+
+let retartGame = true;
+
+// for storing range of number to be guessed
+let rangeNum;
+
+// For storing the number of attempts the user has remaining
+let lives;
+
+// For storing the users guess
+let guess;
+
+// For storing the users rresponse to play again or not play again
+let playAgain;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
