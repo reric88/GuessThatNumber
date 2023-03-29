a7a57bb84bd163c7031a3f646730b1c0c7ec9f37
Eric Collard <ruffyreborn@gmail.com>
2023-03-29 10:18:53 -0500

    added alert, started while loop

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-console.log('ᓚᘏᗢ');
+// console.log('ᓚᘏᗢ');
+
 // Guess That Number
 // Message to be used throughout the project
 const enterNumText = 'Please enter a number greate than zerp:';
@@ -17,8 +18,9 @@ let guess;
 // For storing the users rresponse to play again or not play again
 let playAgain;
 
-
-
+// Starting alert message
+console.log('Welcome to THE GAME. Please click "OK" to start the game.');
+alert('Welcome to THE GAME. Please click "OK" to start the game.')
 
 
 
