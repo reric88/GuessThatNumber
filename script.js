5532b33b2e41c002ba81127495c49f7b8c34bf33
Eric Collard <ruffyreborn@gmail.com>
2023-03-29 10:39:45 -0500

    added prompt to the restartGame while loop to ask and check users entry

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 
 // Guess That Number
 // Message to be used throughout the project
-const enterNumText = 'Please enter a number greate than zerp:';
+const enterNumText = 'Please enter a number greater than zero:';
 
-let retartGame = true;
+let restartGame = true;
 
 // for storing range of number to be guessed
 let rangeNum;
@@ -15,14 +15,39 @@ let lives;
 // For storing the users guess
 let guess;
 
-// For storing the users rresponse to play again or not play again
+// For storing the users response to play again or not play again
 let playAgain;
 
 // Starting alert message
-console.log('Welcome to THE GAME. Please click "OK" to start the game.');
+// console.log('Welcome to THE GAME. Please click "OK" to start the game.');
 alert('Welcome to THE GAME. Please click "OK" to start the game.')
 
+// Game restarts as long ias restartGame is true
+while (restartGame) {
+    // Asks the user to enter a number to set the upper limit for the random number that will be created
+    rangeNum = prompt('Please enter a maximum number for the range');
+    
+    // Using parseInt to attempt to convert the users response into a number value
+    rangeNum = parseInt(rangeNum)
+    
+    // Verifies the users entry for a range number is a number greater than 0
+    // All number, positive or negative, have a default boolean value of true, except for zero, which is false
+    while (!rangeNum || rangeNum < 1) {
+        rangeNum = prompt(enterNumText);
+        rangeNum = parseInt(rangeNum);
+    }
 
+
+
+
+
+
+
+
+
+
+    restartGame = false;
+}
 
 
 
